de9a795d098608e281230a6658787fc2bef4556f
yahya alshomrani <yalshomrani0002@stu.kau.edu.sa>
2016-11-16 20:06:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/yahya/senddatatoserver/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.yahya.senddatatoserver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    EditText message;
+    Button send;
+    RequestQueue queue;
+    String path="/getData.php";
+    String server = "http://192.168.1.30/android";
+    String curl = server+path;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        send = (Button) findViewById(R.id.Send);
+        message = (EditText) findViewById(R.id.editText2);
+
+        queue = Volley.newRequestQueue(this);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, curl+"?op=2&msg="+message.getText().toString(),
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                //System.out.println("I C RESPONSE");
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //System.out.println("ERROR HAPPENED!");
+                    }
+                });
+                message.setText("");
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
+                //queue.start();
+            }
+        });
+
+    }
+}
